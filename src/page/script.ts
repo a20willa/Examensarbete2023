85fd87fd26fb63d9e235cf9efa4abe14ce0aabaa
a20willa <larsson.william2000@gmail.com>
2023-03-13 15:28:16 +0100

    added a table to show requests on the page

--- src/page/script.ts
+++ src/page/script.ts
@@ -23,8 +23,29 @@ async function callGetAllEndpoint() {
 
     // Get response and print them at the end
     const responses = await Promise.all(fetches)
+    const output_table = document.getElementById("output_table")!
+    let columRow = 0
     for (const response of responses) {
-        console.log(response)
+        // Create index attribute
+        const index = document.createElement("td")
+        index.innerText = String(columRow)
+
+        // Create URL attribute
+        const url = document.createElement("td")
+        url.innerText = response.url
+
+        // Create ok attribute
+        const ok = document.createElement("td")
+        ok.innerText = String(response.ok)
+
+        // Create row in table
+        const row = document.createElement("tr")
+        row.append(index, url, ok)
+        output_table.appendChild(row)
+
+        columRow++
     }
+
+    document.getElementById("table_wrapper")!.appendChild(output_table)
     console.log(`END - ${fetches.length} queries was runned successfully`)
 }
