69f1152b24c305fc140fb38eb08371800046a6e3
a20willa <larsson.william2000@gmail.com>
2023-03-16 14:03:05 +0100

    fixed bug where for loop used "in" instead of "of" causing no values to be used within, but the index instead

--- getHAR/getHar.js
+++ getHAR/getHar.js
@@ -1,17 +1,18 @@
 function getNetworkHAR() {
     browser.devtools.network.getHAR().then((har) => {
         // Create object to store relevant data
-        const harString = {values: []}
+        const harString = { values: [] }
 
         // Only get relevant results
-        for(entry in har.entries){
-            if(entry.request.url.includes("getAll")) {
-                harString.values.push({time: entry.time, request: entry.request});
+        for (entry of har.entries) {
+            console.log(entry)
+            if (entry.request.url.includes("getAll")) {
+                harString.values.push({ time: entry.time, request: entry.request });
             }
         }
 
         // Construct and download file
-        const file = new Blob([JSON.stringify(harString)], {type: 'application/json'});
+        const file = new Blob([JSON.stringify(harString)], { type: 'application/json' });
         const a = document.createElement('a');
         a.href = URL.createObjectURL(file);
         a.download = 'geospatial_test_data.json';
@@ -19,13 +20,16 @@ function getNetworkHAR() {
         a.click();
         document.body.removeChild(a);
     });
-  }
-  
-  browser.devtools.panels.create("Get HAR", "icon.png", "panel.html", (panel) => {
+}
+
+browser.devtools.panels.create("Get HAR", "icon.png", "panel.html", (panel) => {
     panel.onShown.addListener((window) => {
-      const button = window.document.createElement("button");
-      button.textContent = "Get Network HAR";
-      button.addEventListener("click", getNetworkHAR);
-      window.document.body.appendChild(button);
+        const button = window.document.createElement("button");
+        button.textContent = "Get Network HAR";
+        button.addEventListener("click", getNetworkHAR);
+        // Check if button exist
+        if (!window.document.body.contains(button)) {
+            window.document.body.appendChild(button);
+        }
     });
-  });
+});
