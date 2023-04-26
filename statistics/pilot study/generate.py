ab3c8d267ff33ea2bf1dc8457fc8aefda012b92a
a20willa <larsson.william2000@gmail.com>
2023-04-26 13:25:33 +0200

    changed method to get anova

--- statistics/pilot study/generate.py
+++ statistics/pilot study/generate.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 import json
 import numpy as np
-from scipy.stats import f_oneway, sem
+import scipy.stats as stats
+import numpy as np
+import pandas as pd
+from io import StringIO
 
 def generateLineDiagram(database):
     """
@@ -41,8 +44,7 @@ def generateLineDiagram(database):
     # Show the plot
     # Customize the x-axis tick locations and labels
     plt.xticks(np.arange(0, global_time_values + 1, 10))
-    plt.legend(["Point", "LineString", "MultiLineString"], loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.tight_layout()
+    plt.legend(["Point", "LineString", "MultiLineString"], loc='upper right')
     plt.savefig('./figures/{}_linechart.png'.format(database))
 
 def generateBarDiagram(database):
@@ -58,9 +60,9 @@ def generateBarDiagram(database):
 
     # Set the title of the plot
     if database == "mysql":
-        plt.title("MySQL standard error in milliseconds over 100 attempt")
+        plt.title("MySQL standard deviation in milliseconds over 100 attempt")
     elif database == "mongodb":
-        plt.title("MongoDB standard error in milliseconds over 100 attempt")
+        plt.title("MongoDB standard deviation in milliseconds over 100 attempt")
 
     # Set the labels of the plot
     plt.xlabel("Amount of requests")
@@ -83,7 +85,7 @@ def generateBarDiagram(database):
         y.append(np.mean(time_values))
 
         # Standard error
-        se.append(np.std(time_values, ddof=1) / np.sqrt(np.size(time_values)))
+        se.append(np.std(time_values))
 
     # Show the plot
     # Customize the x-axis tick locations and labels
@@ -98,7 +100,63 @@ def generateBarDiagram(database):
     plt.errorbar(x, y, yerr=se, fmt='none', color='black', capsize=5)
     # Fix colors
     plt.bar(x,y, color=['#1f77b4', '#ff7f0e', '#2ca02c'])
-    plt.savefig('./figures/{}_SE.png'.format(database))
+    plt.savefig('./figures/{}_STD.png'.format(database))
+
+def anova(*data):  # * indicates, 0, 1 , 2 .. arguments
+    if len(data) == 2:
+        statistic, pvalue = stats.f_oneway(data[0], data[1])
+    elif len(data) == 3:
+        statistic, pvalue = stats.f_oneway(data[0], data[1], data[2])
+    elif len(data) == 4:
+        statistic, pvalue = stats.f_oneway(data[0], data[1], data[2], data[3])
+
+    print("ANOVA Statistic " + str(statistic)+ " and p-value "+ str(pvalue))
+
+    if pvalue < 0.05:  # alpha = 0.05
+        return True
+    else:
+        return False
+
+def exampleAnova():
+    # Get the pilot study files
+    points_mongodb = ["./mysql/point/geospatial_test_data.json", "./mysql/linestring/geospatial_test_data.json", "./mysql/multilinestring/geospatial_test_data.json"]
+    points_mysql = ["./mongodb/point/geospatial_test_data.json", "./mongodb/linestring/geospatial_test_data.json", "./mongodb/multilinestring/geospatial_test_data.json"]
+
+    # Loop through the files - MongoDB
+    for file in range(len(points_mongodb)):
+        combined_csv_mongodb = ""
+        combined_csv_mysql = ""
+
+        # Convert json to csv and read the csv
+        with open(points_mongodb[file], 'r') as j:
+            json_file = json.loads(j.read())
+        
+            # Loop through the values
+            for i in json_file['values']:
+                combined_csv_mysql += str(i['time']) + "\n"
+
+        # Convert json to csv and read the csv
+        with open(points_mysql[file], 'r') as j:
+            json_file = json.loads(j.read())
+        
+            # Loop through the values
+            for i in json_file['values']:
+                combined_csv_mongodb += str(i['time']) + "\n"
+
+        # Read the csv
+        df = pd.read_csv( StringIO(combined_csv_mongodb), header=None, names=['time'])
+        df2 = pd.read_csv( StringIO(combined_csv_mysql), header=None, names=['time'])
+        
+        # Print the results
+        print("Results for type: " + points_mongodb[file].split("/")[2].split(".")[0].upper())
+
+        # Run Anova on data groups
+        if (anova(df['time'], df2['time'])):
+            print("The means are different")
+        else:
+            print("No differences in means")
+
+        print("")
 
 def performAnovaTest():
     # Define the data for MongoDB and MySQL
@@ -147,13 +205,12 @@ def performAnovaTest():
     print("F statistic:", f_stat)
     print("p-value:", p_val)
 
-
 # Anova tests
-performAnovaTest()
+exampleAnova()
 
 # Line diagrams
-generateLineDiagram("mongodb")
-generateLineDiagram("mysql")
+#generateLineDiagram("mongodb")
+#generateLineDiagram("mysql")
 # Bar diagrams
-generateBarDiagram("mongodb")
-generateBarDiagram("mysql")
+#generateBarDiagram("mongodb")
+#generateBarDiagram("mysql")
